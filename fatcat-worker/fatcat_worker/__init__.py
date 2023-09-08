3a27b7f17864386a8891485d4d1879d8f2c951ec
Yotamefr <yotamefr@gmail.com>
2023-09-08 17:15:28 +0300

    Made fatcat submodules

--- fatcat-worker/fatcat_worker/__init__.py
+++
@@ -1,9 +0,0 @@
-import argparse
-from dotenv import load_dotenv
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--env", type=str, help="The path to the .env file")
-args = parser.parse_args()
-
-load_dotenv(args.env)
-
-from .worker import Worker
